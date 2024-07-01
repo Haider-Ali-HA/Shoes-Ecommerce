498241dd978d05e0cff3376cc0211115e299691e
Haider Ali <haideraliatish500@gmail.com>
2024-07-01 21:20:53 +0500

    hero section ready

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -5,25 +5,30 @@ import React, { useState } from "react";
 
 const Hero = () => {
   return (
-    <div className="relative  w-full h-screen  flex items-center justify-center gap-44">
+    <div className="relative px-5  w-full h-screen  flex items-center justify-center ">
       <img
         className="absolute  opacity-70 h-full w-full"
         src="./image/1bg.jpg"
         alt=""
       />
       <div className="inset"></div>
-      <div className="relative font-poppins font-bold text-8xl">
-        <h1 className="">Find Your </h1>
-        <h1 className=" text-[#ff0000]">Perfect Pair </h1>
+      <div className="relative flex flex-col justify-center items-center sm:items-start  font-poppins font-bold sm:w-3/5 lg:w-2/5 ">
+        <h1 className="text-3xl md:text-4xl lg:text-7xl">Find Your </h1>
+        <h1 className="text-3xl md:text-4xl lg:text-7xl text-[#ff0000]">Perfect Pair </h1>
+        <p className="text-xs md:text-base mt-2 text-center sm:text-start">
+          Discover the latest trends in footwear, designed for both comfort and
+          style. Step into a world where fashion meets function, and find your
+          perfect pair today!
+        </p>
         <div className="text-base font-semibold my-4">
-          <button className="bg-[#000000] border border-[#000000] hover:bg-transparent hover:text-black rounded-full w-32 h-12 text-white transition-all duration-500">
+          <button className="bg-[#000000] border border-[#000000] text-xs md:text-base hover:bg-transparent hover:text-black rounded-full h-10 w-24 md:w-32  md:h-12 text-white transition-all duration-500">
             Shop Now
           </button>
         </div>
       </div>
-      <div className=" relative flex items-center justify-center ">
+      <div className=" relative hidden sm:flex items-center justify-center ">
         <img
-          className="duration-500 ease-in-out"
+          className="w-56 md:w-3/5 lg:w-2/3"
           src="./image/redShoesHero.svg"
           alt="hero section image"
         />
