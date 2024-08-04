918d791c0c3af75734b8f35fd0b7db5555b8b38a
Haider Ali <haideraliatish500@gmail.com>
2024-08-04 18:34:11 +0500

    adjusting cart item descrement and increment

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -8,32 +8,40 @@ const CartItems = () => {
   const { cart } = useCartContext();
   //   console.log("cart items", cart);
   //   console.log("cart items data", cart.quantity);
-  const [quantity, setQuantity] = useState(cart.quantity);
+  // const [quantity, setQuantity] = useState(cart.quantity);
 
-  const IncreaseQuantity = () => {
-    quantity > 0 ? setQuantity(quantity + 1) : setQuantity(1);
-  };
-  const DecreaseQuantity = () => {
-    quantity > 1 ? setQuantity(quantity - 1) : setQuantity(1);
-  };
+  // const IncreaseQuantity = () => {
+  //   quantity > 0 ? setQuantity(quantity + 1) : setQuantity(1);
+  // };
+  // const DecreaseQuantity = () => {
+  //   quantity > 1 ? setQuantity(quantity - 1) : setQuantity(1);
+  // };
 
   return (
     <div className="flex  flex-col gap-8">
       {cart.map((item) => {
+        const [quantity, setQuantity] = useState(item.quantity);
+        const IncreaseQuantity = () => {
+          quantity > 0 ? setQuantity(quantity + 1) : setQuantity(1);
+        };
+        const DecreaseQuantity = () => {
+          quantity > 1 ? setQuantity(quantity - 1) : setQuantity(1);
+        };
+
         return (
           <div
             key={item.id}
-            className="grid grid-cols-3 md:grid-cols-5   text-center text-xl font-semibold"
+            className="grid grid-cols-4 md:grid-cols-6   text-center text-xl font-semibold"
           >
-            <div className="col-span-1 flex gap-3 ">
+            <div className="col-span-2  flex justify-center items-center gap-3 ">
               <img
-                className="w-16 h-14  rounded"
+                className="w-12 md:w-16 h-10 md:h-14  rounded"
                 src={item.image}
                 alt={item.name}
               />
-              <span className="flex flex-col ">
-                <p className="text-lg">{item.name}</p>
-                <span className="flex items-center text-lg">
+              <span className="flex flex-col justify-center ">
+                <p className="text-sm text-start md:text-lg ">{item.name}</p>
+                <span className="flex items-center text-sm md:text-lg">
                   <p>Color:</p>
                   <p
                     className="h-4 w-4  border shadow-2xl rounded-full font-bold  hover:opacity-70 cursor-pointer flex items-center justify-center text-4xl text-white mx-2 "
@@ -42,18 +50,17 @@ const CartItems = () => {
                 </span>
               </span>
             </div>
-            <h1 className="col-span-1 m-auto hidden md:block ">
-           
+            <h1 className="m-auto col-span-1 hidden md:block  text-lg">
               <CurrencyConvert price={item.price} />
             </h1>
-            <h1 className="col-span-1  m-auto flex gap-3 justify-center">
+            <h1 className=" m-auto col-span-1 flex gap-3 justify-center text-sm md:text-lg">
               <span
                 className="hover:bg-gray-700 hover:text-white rounded-full p-1"
                 onClick={() => DecreaseQuantity()}
               >
                 <FaMinus />
               </span>
-              <span>{item.quantity}</span>
+              <span>{quantity}</span>
               <span
                 className="hover:bg-gray-700 hover:text-white rounded-full p-1"
                 onClick={() => IncreaseQuantity()}
@@ -61,11 +68,11 @@ const CartItems = () => {
                 <FaPlus />
               </span>
             </h1>
-            <h1 className="col-span-1 m-auto  hidden md:block ">
+            <h1 className="m-auto col-span-1  hidden md:block text-lg">
               {" "}
               <CurrencyConvert price={item.price * item.quantity} />
             </h1>
-            <h1 className="col-span-1  m-auto   text-2xl text-[#c90404]">
+            <h1 className=" m-auto col-span-1   text-xl md:text-2xl text-[#c90404]">
               <MdDelete />
             </h1>
           </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,15 +4,15 @@ import CartItems from "../components/CartItems";
 const Cart = () => {
   return (
     <div className="mt-20 h-full w-full flex items-center justify-center flex-col gap-3 py-32">
-      <div className="grid grid-cols-3 md:grid-cols-3 gap-5 w-4/6 text-center text-xl font-semibold">
-        <h1 className="col-span-1 ">Items</h1>
-        <h1 className="col-span-1 ">Price</h1>
+      <div className="grid grid-cols-4 md:grid-cols-6 gap-5 w-5/6 lg:w-4/6 text-center text-sm md:text-xl font-semibold">
+        <h1 className="col-span-2 ">Items</h1>
+        <h1 className="col-span-1 hidden md:block ">Price</h1>
         <h1 className="col-span-1 ">Quantity</h1>
-        <h1 className="col-span-1 ">SubTotal</h1>
+        <h1 className="col-span-1 hidden md:block ">SubTotal</h1>
         <h1 className="col-span-1 ">Remove</h1>
       </div>
-      <hr className="h-2 w-4/6 border-black " />
-      <div className=" w-4/6">
+      <hr className="h-2 w-5/6 lg:w-4/6 border-black " />
+      <div className=" w-5/6 lg:w-4/6">
 
         <CartItems />
       </div>
